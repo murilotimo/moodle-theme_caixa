27df317c60c9f2a270de617369d0fa95eb3758b3
Mike Grant <=>
2014-10-29 15:43:17 +0000

    Fixes #43

--- config.php
+++ config.php
@@ -38,7 +38,7 @@ $THEME->name = 'bcu';
 //
 $THEME->doctype = 'html5';
 $THEME->parents = array('bootstrapbase');
-$THEME->sheets = array('custom','blocks','menu','course','slider','extras','font-awesome','zoom');
+$THEME->sheets = array('custom','blocks','menu','course','slider','extras','zoom');
 $THEME->supportscssoptimisation = false;
 $THEME->yuicssmodules = array();
 
