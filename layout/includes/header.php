ece3abaea112e76bfa58171ffeb5118168d227a6
Mike Grant <=>
2014-11-05 10:52:15 +0000

    Fixes #48

--- layout/includes/header.php
+++ layout/includes/header.php
@@ -41,7 +41,7 @@ echo $OUTPUT->doctype();
     
 </head>
 
-<body <?php echo $OUTPUT->body_attributes('two-column'); ?>>
+<body <?php echo $OUTPUT->body_attributes(array('two-column', $setzoom)); ?>>
 
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 <div id="page" class="container-fluid <?php echo "$setfull"; ?>">
