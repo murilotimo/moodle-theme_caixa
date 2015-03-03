22d5eb116911fee6c68ede027232611c35f981fc
Mike Grant <michael.grant@bcu.ac.uk>
2015-03-03 08:51:51 +0000

    BCU-121: Fixes #121

--- layout/includes/header.php
+++ layout/includes/header.php
@@ -149,7 +149,7 @@ echo $OUTPUT->doctype();
             <form action="<?php p($CFG->wwwroot) ?>/course/search.php">
                 <label class="hidden" for="search-1" style="display: none;">Search iCity</label>
                 <div class="search-box grey-box bg-white clear-fix">
-                    <input placeholder="Search courses" accesskey="6" class="search_tour bg-white no-border left search-box__input ui-autocomplete-input" type="text" name="search" id="search-1" autocomplete="off">
+                    <input placeholder="<?php echo get_string("searchcourses")?>" accesskey="6" class="search_tour bg-white no-border left search-box__input ui-autocomplete-input" type="text" name="search" id="search-1" autocomplete="off">
                     <button type="submit" class="no-border bg-white pas search-box__button"><abbr class="fa fa-search"></abbr></button>
                 </div>
             </form>
