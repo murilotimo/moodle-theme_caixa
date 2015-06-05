f6b9305b6284266e54f3d671dd29152961cb5c81
Matthew Anguige <mattanguige@gmail.com>
2015-06-05 09:47:34 +0100

    Slider caption text box now clickable if present.

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -51,62 +51,62 @@ if ($hasp1 || $hasp2 || $hasp3 || $hasp4 || $hasp5) { ?>
 <div class="container slidewrap">
 <div id="main-slider" class="flexslider">
 <ul class="slides">
-<?php 
+<?php
     if ($hasp1) {
  ?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p1url)) { echo $PAGE->theme->settings->p1url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p1', 'p1')."' alt='p1'/>";?></a>
+<a href="<?php if (!empty($PAGE->theme->settings->p1url)) { echo $PAGE->theme->settings->p1url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p1', 'p1')."' alt='p1'/>";?>
 <?php if ($haspcap1) { ?>
 <div class="flex-caption">
-<?php echo $OUTPUT->get_setting('1cap', 'format_html');?>
-</div>
+<?php echo $OUTPUT->get_setting('p1cap', 'format_html');?>
+</div></a>
 <?php } ?>
-</li>
+</a></li>
 <?php } ?>
 
 <?php if ($hasp2) { ?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p2url)) {echo $PAGE->theme->settings->p2url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p2', 'p2')."' alt='p2' />";?></a>
+<a href="<?php if (!empty($PAGE->theme->settings->p2url)) {echo $PAGE->theme->settings->p2url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p2', 'p2')."' alt='p2' />";?>
 <?php if ($haspcap2) { ?>
 <div class="flex-caption">
 <?php echo $OUTPUT->get_setting('p2cap', 'format_html');?>
-</div>
+</div></a>
 <?php } ?>
-</li>
+</a></li>
 <?php } ?>
 
 
 <?php if ($hasp3) { ?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p3url)) {echo $PAGE->theme->settings->p3url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p3', 'p3')."' alt='p3' />";?></a>
+<a href="<?php if (!empty($PAGE->theme->settings->p3url)) {echo $PAGE->theme->settings->p3url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p3', 'p3')."' alt='p3' />";?>
 <?php if ($haspcap3) { ?>
 <div class="flex-caption">
 <?php echo $OUTPUT->get_setting('p3cap', 'format_html');?>
-</div>
+</div></a>
 <?php } ?>
-</li>
+</a></li>
 <?php } ?>
 
 <?php if ($hasp4) { ?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p4url)) {echo $PAGE->theme->settings->p4url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p4', 'p4')."' alt='p4' />";?></a>
+<a href="<?php if (!empty($PAGE->theme->settings->p4url)) {echo $PAGE->theme->settings->p4url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p4', 'p4')."' alt='p4' />";?>
 <?php if ($haspcap4) { ?>
 <div class="flex-caption">
 <?php echo $OUTPUT->get_setting('p4cap', 'format_html');?>
-</div>
+</div></a>
 <?php } ?>
-</li>
+</a></li>
 <?php } ?>
 
 <?php if ($hasp5) { ?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p5url)) {echo $PAGE->theme->settings->p5url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p5', 'p5')."' alt='p5' />";?></a>
+<a href="<?php if (!empty($PAGE->theme->settings->p5url)) {echo $PAGE->theme->settings->p5url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p5', 'p5')."' alt='p5' />";?>
 <?php if ($haspcap5) { ?>
 <div class="flex-caption">
 <?php echo $OUTPUT->get_setting('p5cap', 'format_html');?>
-</div>
+</div></a>
 <?php } ?>
-</li>
+</a></li>
 <?php } ?>
 	</ul>
 </div>
