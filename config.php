3ab4be188052268a0e5819b287601d88bfe2abda
unknown <ac1216@CUTBM3244EF.coventry.ac.uk>
2015-10-17 23:07:31 +0100

    Revert "BCU 2.8 menu changes"
    This reverts commit 3e36e1bda7c803591776898168fe83dd206539db.

--- config.php
+++ config.php
@@ -100,12 +100,12 @@ $THEME->layouts = array(
         'regions' => array('side-post'),
         'defaultregion' => 'side-post',
     ),
+    // Login page.
     'login' => array(
         'file' => 'columns1.php',
         'regions' => array(),
-        'options' => array('langmenu' => true),
+        'options' => array('langmenu' => true, 'nonavbar' => true),
     ),
-
     // Pages that appear in pop-up windows - no navigation, no blocks, no header.
     'popup' => array(
         'file' => 'columns1.php',
@@ -155,7 +155,6 @@ $THEME->layouts = array(
         'defaultregion' => 'side-post'
     ),
 );
-
 
 $THEME->csspostprocess = 'theme_bcu_process_css';
 $THEME->blockrtlmanipulations = array(
