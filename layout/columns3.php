murilotimo/moodle-theme_caixa

commit b71209a23aa1e10ab3294525668afa313da08593

Pass CodeChecker

layout/columns3.php
@@ -24,7 +24,7 @@
  *
  */
 
-require_once(dirname(__FILE__) . '/includes/header.php'); 
+require_once(dirname(__FILE__) . '/includes/header.php');
 $left = theme_bcu_get_block_side();
 ?>
 <div id="page" class="container-fluid">
@@ -65,11 +65,11 @@ $left = theme_bcu_get_block_side();
                     <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
                     <?php echo $OUTPUT->navbar(); ?>
                 </div>
-                <?php
-                    if($left == 1) {
-                        echo $OUTPUT->blocks('side-post', 'span3 desktop-first-column');
-                    }
-                ?>
+<?php
+if ($left == echo) {
+    $OUTPUT->blocks('side-post', 'span3 desktop-first-column');
+}
+?>
                 <section id="region-main" class="span8 pull-right">
                     <?php
                     echo $OUTPUT->course_content_header();
@@ -80,11 +80,11 @@ $left = theme_bcu_get_block_side();
                 <?php echo $OUTPUT->blocks('side-pre', 'span4 desktop-first-column'); ?>
             </div>
         </div>
-        <?php
-            if($left == 0) {
-                echo $OUTPUT->blocks('side-post', 'span3');
-            }
-        ?>
+<?php
+if ($left == 0) {
+    echo $OUTPUT->blocks('side-post', 'span3');
+}
+?>
     </div>
 
-    <?php require_once(dirname(__FILE__) . '/includes/footer.php'); ?>
+<?php require_once(dirname(__FILE__) . '/includes/footer.php');

layout/frontpage.php
@@ -58,287 +58,370 @@ $hasmarket11 = (!empty($PAGE->theme->settings->market11));
 $hasmarket12 = (!empty($PAGE->theme->settings->market12));
 
 if ($hasp1 || $hasp2 || $hasp3 || $hasp4 || $hasp5) {
-	 
     if (!empty($PAGE->theme->settings->sliderfullscreen)) {
         echo '<div class="slidewrap';
     } else {
         echo '<div class="container slidewrap';
     }
-	 
-if (!empty($PAGE->theme->settings->slideroption2)) {echo " slidestyle2"; } ?>">
+
+    if (!empty($PAGE->theme->settings->slideroption2)) {
+        echo " slidestyle2";
+    } ?>">
+
 <div id="main-slider" class="flexslider">
 <ul class="slides">
 <?php
     if ($hasp1) {
  ?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p1url)) { echo $PAGE->theme->settings->p1url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p1', 'p1')."' alt='p1'/>";?>
-<?php if ($haspcap1) { ?>
-<div class="flex-caption">
-<?php echo $OUTPUT->get_setting('p1cap', 'format_html');?>
-</div></a>
-<?php } ?>
+<a href="<?php
+        if (!empty($PAGE->theme->settings->p1url)) {
+            echo $PAGE->theme->settings->p1url;
+        } else {
+            echo "#";
+        } ?>">
+<?php echo "<img src='".$PAGE->theme->setting_file_url('p1', 'p1')."' alt='p1'/>";?>
+<?php
+        if ($haspcap1) { ?>
+              <div class="flex-caption">
+                  <?php echo $OUTPUT->get_setting('p1cap', 'format_html');?>
+              </div>
+</a>
+<?php
+        }
+?>
 </a></li>
-<?php } ?>
+<?php
+    }
+?>
 
-<?php if ($hasp2) { ?>
+<?php
+    if ($hasp2) {
+?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p2url)) {echo $PAGE->theme->settings->p2url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p2', 'p2')."' alt='p2' />";?>
-<?php if ($haspcap2) { ?>
-<div class="flex-caption">
-<?php echo $OUTPUT->get_setting('p2cap', 'format_html');?>
-</div></a>
-<?php } ?>
+<a href="<?php
+        if (!empty($PAGE->theme->settings->p2url)) {
+            echo $PAGE->theme->settings->p2url;
+        } else {
+            echo "#";
+        } ?>">
+    <?php echo "<img src='".$PAGE->theme->setting_file_url('p2', 'p2')."' alt='p2' />";?>
+
+<?php
+        if ($haspcap2) {
+?>
+        <div class="flex-caption">
+            <?php echo $OUTPUT->get_setting('p2cap', 'format_html');?>
+        </div></a>
+<?php
+        }
+?>
 </a></li>
-<?php } ?>
+<?php
+    }
+?>
 
-
-<?php if ($hasp3) { ?>
+<?php
+    if ($hasp3) {
+?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p3url)) {echo $PAGE->theme->settings->p3url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p3', 'p3')."' alt='p3' />";?>
-<?php if ($haspcap3) { ?>
-<div class="flex-caption">
-<?php echo $OUTPUT->get_setting('p3cap', 'format_html');?>
-</div></a>
-<?php } ?>
-</a></li>
-<?php } ?>
+<a href="<?php
+        if (!empty($PAGE->theme->settings->p3url)) {
+            echo $PAGE->theme->settings->p3url;
+        } else {
+            echo "#";
+        } ?>">
+    <?php echo "<img src='".$PAGE->theme->setting_file_url('p3', 'p3')."' alt='p3' />";?>
 
-<?php if ($hasp4) { ?>
-<li>
-<a href="<?php if (!empty($PAGE->theme->settings->p4url)) {echo $PAGE->theme->settings->p4url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p4', 'p4')."' alt='p4' />";?>
-<?php if ($haspcap4) { ?>
-<div class="flex-caption">
-<?php echo $OUTPUT->get_setting('p4cap', 'format_html');?>
-</div></a>
-<?php } ?>
+<?php
+        if ($haspcap3) { ?>
+        <div class="flex-caption">
+            <?php echo $OUTPUT->get_setting('p3cap', 'format_html');?>
+        </div></a>
+<?php
+        }
+?>
 </a></li>
-<?php } ?>
+<?php
+    }
+?>
 
-<?php if ($hasp5) { ?>
+<?php
+    if ($hasp4) {
+?>
 <li>
-<a href="<?php if (!empty($PAGE->theme->settings->p5url)) {echo $PAGE->theme->settings->p5url; } else { echo "#";} ?>"><?php echo "<img src='".$PAGE->theme->setting_file_url('p5', 'p5')."' alt='p5' />";?>
-<?php if ($haspcap5) { ?>
-<div class="flex-caption">
-<?php echo $OUTPUT->get_setting('p5cap', 'format_html');?>
-</div></a>
-<?php } ?>
+<a href="<?php
+        if (!empty($PAGE->theme->settings->p4url)) {
+            echo $PAGE->theme->settings->p4url;
+        } else {
+            echo "#";
+        } ?>">
+    <?php echo "<img src='".$PAGE->theme->setting_file_url('p4', 'p4')."' alt='p4' />";?>
+
+<?php
+        if ($haspcap4) { ?>
+         <div class="flex-caption">
+             <?php echo $OUTPUT->get_setting('p4cap', 'format_html');?>
+         </div></a>
+<?php
+        }
+?>
 </a></li>
-<?php } ?>
+<?php
+    }
+?>
+
+<?php
+    if ($hasp5) {
+?>
+<li>
+<a href="<?php
+        if (!empty($PAGE->theme->settings->p5url)) {
+            echo $PAGE->theme->settings->p5url;
+        } else {
+            echo "#";
+        } ?>">
+    <?php echo "<img src='".$PAGE->theme->setting_file_url('p5', 'p5')."' alt='p5' />";?>
+
+<?php
+        if ($haspcap5) { ?>
+        <div class="flex-caption">
+            <?php echo $OUTPUT->get_setting('p5cap', 'format_html');?>
+        </div></a>
+<?php
+        }
+?>
+</a></li>
+<?php
+    }
+?>
 	</ul>
 </div>
 </div>
 
-<?php } ?>
+<?php
+}
+?>
 
-<?php if (!empty($PAGE->theme->settings->infobox)) {
-	 
-		if (!empty($PAGE->theme->settings->infoboxfullscreen)) {
-	        echo '<div id="theinfo">';
-	    } else {
-	        echo '<div id="theinfo" class="container">';
-	    } 	?>
-	
+<?php
+if (!empty($PAGE->theme->settings->infobox)) {
+    if (!empty($PAGE->theme->settings->infoboxfullscreen)) {
+        echo '<div id="theinfo">';
+    } else {
+        echo '<div id="theinfo" class="container">';
+    }
+?>
+
         <div id="theinfo-internal">
             <div class="row-fluid">
                 <?php echo $OUTPUT->get_setting('infobox', 'format_html'); ?>
             </div>
         </div>
     </div>
-<?php } ?>
-
-
+<?php
+}
+?>
 
 <?php
-// Display marketing blocks
-if (!empty($PAGE->theme->settings->frontpagemarketenabled)) {
 
-if (!empty($PAGE->theme->settings->frontpagemarketoption)) {
-$extramarketclass = "covtiles";
-} else {
-$extramarketclass = "";
-}
+// Display marketing blocks.
+if (!empty($PAGE->theme->settings->frontpagemarketenabled)) {
+    if (!empty($PAGE->theme->settings->frontpagemarketoption)) {
+        $extramarketclass = "covtiles";
+    } else {
+        $extramarketclass = "";
+    }
 ?>
 
 <div id="marketblocks" class="container <?php echo "$extramarketclass"; ?>">
 <div class="row-fluid">
 <?php
-$marketclass = " ";
-if ($hasmarket1 && !$hasmarket2 && !$hasmarket3 && !$hasmarket4) {
-	$marketclass = "span12 $extramarketclass";
-}
+    $marketclass = " ";
+    if ($hasmarket1 && !$hasmarket2 && !$hasmarket3 && !$hasmarket4) {
+        $marketclass = "span12 $extramarketclass";
+    }
 
-if ($hasmarket1 && $hasmarket2 && !$hasmarket3 && !$hasmarket4) {
-	$marketclass = "span6 $extramarketclass";
-}
+    if ($hasmarket1 && $hasmarket2 && !$hasmarket3 && !$hasmarket4) {
+        $marketclass = "span6 $extramarketclass";
+    }
 
-if ($hasmarket1 && $hasmarket2 && $hasmarket3 && !$hasmarket4) {
-	$marketclass = "span4 $extramarketclass";
-}
+    if ($hasmarket1 && $hasmarket2 && $hasmarket3 && !$hasmarket4) {
+        $marketclass = "span4 $extramarketclass";
+    }
 
-if ($hasmarket1 && $hasmarket2 && $hasmarket3 && $hasmarket4) {
-	$marketclass = "span3 $extramarketclass";
-}
+    if ($hasmarket1 && $hasmarket2 && $hasmarket3 && $hasmarket4) {
+        $marketclass = "span3 $extramarketclass";
+    }
 ?>
-    <?php if ($hasmarket1) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market1', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
-
-     <?php if ($hasmarket2) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market2', 'format_html');?>
-
-    </div>
-     <?php } ?>
-
+    <?php
+    if ($hasmarket1) { ?>
+        <div class="<?php echo "$marketclass";?> first">
 
 <?php
-if ($hasmarket3) {
+        echo $OUTPUT->get_setting('market1', 'format_html');
+?>
+
+        </div>
+    <?php
+    }
+    ?>
+
+    <?php
+    if ($hasmarket2) {
+    ?>
+        <div class="<?php echo "$marketclass";?> first">
+    <?php echo $OUTPUT->get_setting('market2', 'format_html'); ?>
+        </div>
+     <?php
+    }
+    ?>
+
+<?php
+    if ($hasmarket3) {
 ?>
     <div class="<?php echo "$marketclass";?> first">
         <?php echo $OUTPUT->get_setting('market3', 'format_html');?>
     </div>
 <?php
-}
+    }
 ?>
 
-
 <?php
-if ($hasmarket4) {
+    if ($hasmarket4) {
 ?>
     <div class="<?php echo "$marketclass";?> first">
 	   <?php echo $OUTPUT->get_setting('market4', 'format_html');?>
     </div>
 <?php
-}
+    }
+?>
+</div>
+
+ <?php
+    if ($hasmarket5) {
+?>
+    <div class="row-fluid marketrow">
+ <?php
+        $marketclass = " ";
+        if ($hasmarket5 && !$hasmarket6 && !$hasmarket7 && !$hasmarket8) {
+            $marketclass = "span12 $extramarketclass";
+        }
+
+        if ($hasmarket5 && $hasmarket6 && !$hasmarket7 && !$hasmarket8) {
+            $marketclass = "span6 $extramarketclass";
+        }
+
+        if ($hasmarket5 && $hasmarket6 && $hasmarket7 && !$hasmarket8) {
+            $marketclass = "span4 $extramarketclass";
+        }
+
+        if ($hasmarket5 && $hasmarket6 && $hasmarket7 && $hasmarket8) {
+            $marketclass = "span3 $extramarketclass";
+        }
+?>
+
+<?php
+        if ($hasmarket5) {
+?>
+            <div class="<?php echo "$marketclass";?> first">
+                <?php echo $OUTPUT->get_setting('market5', 'format_html');?>
+            </div>
+<?php
+        }
+?>
+
+<?php
+        if ($hasmarket6) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+	               <?php echo $OUTPUT->get_setting('market6', 'format_html');?>
+            </div>
+<?php
+        }
+?>
+
+<?php
+        if ($hasmarket7) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+	               <?php echo $OUTPUT->get_setting('market7', 'format_html');?>
+            </div>
+    <?php
+        }
+?>
+
+<?php
+        if ($hasmarket8) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+	               <?php echo $OUTPUT->get_setting('market8', 'format_html');?>
+            </div>
+<?php
+        }
+?>
+</div>
+<?php
+    }
+    // End 2nd row of blocks.
 ?>
 
 
- </div>
+<?php
+    if ($hasmarket9) { ?>
+        <div class="row-fluid marketrow">
+            <?php $marketclass = " ";
+        if ($hasmarket9 && !$hasmarket10 && !$hasmarket11 && !$hasmarket12) {
+            $marketclass = "span12 $extramarketclass";
+        }
 
- <?php
- if ($hasmarket5) { ?>
- <div class="row-fluid marketrow">
- <?php
-$marketclass = " ";
-if ($hasmarket5 && !$hasmarket6 && !$hasmarket7 && !$hasmarket8) {
-	$marketclass = "span12 $extramarketclass";
-}
+        if ($hasmarket9 && $hasmarket10 && !$hasmarket11 && !$hasmarket12) {
+               $marketclass = "span6 $extramarketclass";
+        }
 
-if ($hasmarket5 && $hasmarket6 && !$hasmarket7 && !$hasmarket8) {
-	$marketclass = "span6 $extramarketclass";
-}
+        if ($hasmarket9 && $hasmarket10 && $hasmarket11 && !$hasmarket12) {
+                $marketclass = "span4 $extramarketclass";
+        }
 
-if ($hasmarket5 && $hasmarket6 && $hasmarket7 && !$hasmarket8) {
-	$marketclass = "span4 $extramarketclass";
-}
-
-if ($hasmarket5 && $hasmarket6 && $hasmarket7 && $hasmarket8) {
-	$marketclass = "span3 $extramarketclass";
-}
+        if ($hasmarket9 && $hasmarket10 && $hasmarket11 && $hasmarket12) {
+                $marketclass = "span3 $extramarketclass";
+        }
 ?>
 
-	<?php if ($hasmarket5) { ?>
-    <div class="<?php echo "$marketclass";?> first">
+<?php
+        if ($hasmarket9) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+                <?php echo $OUTPUT->get_setting('market9', 'format_html');?>
+            </div>
+<?php
+        }
 
-	<?php echo $OUTPUT->get_setting('market5', 'format_html');?>
+        if ($hasmarket10) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+	               <?php echo $OUTPUT->get_setting('market10', 'format_html');?>
+            </div>
+<?php
+        }
 
-    </div>
-    <?php } ?>
+        if ($hasmarket11) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+	            <?php echo $OUTPUT->get_setting('market11', 'format_html');?>
+            </div>
+<?php
+        }
 
-    <?php if ($hasmarket6) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market6', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
- 	<?php if ($hasmarket7) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market7', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
-    <?php if ($hasmarket8) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market8', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
- </div>
- <?php } //end 2nd row of blocks ?>
-
-
- <?php
- if ($hasmarket9) { ?>
- <div class="row-fluid marketrow">
- <?php
-$marketclass = " ";
-if ($hasmarket9 && !$hasmarket10 && !$hasmarket11 && !$hasmarket12) {
-	$marketclass = "span12 $extramarketclass";
-}
-
-if ($hasmarket9 && $hasmarket10 && !$hasmarket11 && !$hasmarket12) {
-	$marketclass = "span6 $extramarketclass";
-}
-
-if ($hasmarket9 && $hasmarket10 && $hasmarket11 && !$hasmarket12) {
-	$marketclass = "span4 $extramarketclass";
-}
-
-if ($hasmarket9 && $hasmarket10 && $hasmarket11 && $hasmarket12) {
-	$marketclass = "span3 $extramarketclass";
-}
+        if ($hasmarket12) { ?>
+            <div class="<?php echo "$marketclass";?> first">
+                <?php echo $OUTPUT->get_setting('market12', 'format_html');?>
+            </div>
+<?php
+        }
 ?>
 
-	<?php if ($hasmarket9) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market9', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
-    <?php if ($hasmarket10) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market10', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
- 	<?php if ($hasmarket11) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market11', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
-    <?php if ($hasmarket12) { ?>
-    <div class="<?php echo "$marketclass";?> first">
-
-	<?php echo $OUTPUT->get_setting('market12', 'format_html');?>
-
-    </div>
-    <?php } ?>
-
  </div>
- <?php } // End 3rd row of blocks. 
- 
- } // End marketing blocks.
- 
+ <?php
+    }
+    // End 3rd row of blocks.
+
+}
+// End marketing blocks.
+
  ?>
 
  </div>
@@ -364,25 +447,35 @@ if (!empty($PAGE->theme->settings->infobox2)) {
             <nav class="breadcrumb-button"><?php echo $OUTPUT->page_heading_button(); ?></nav>
             <?php echo $OUTPUT->navbar(); ?>
     </div>
-        <?php
-        if($left == 1) {
-            echo $OUTPUT->blocks('side-post', 'span3 desktop-first-column');
-        }
-        ?>
-        <section id="region-main" class="span9 <?php if ($left) { echo ' '; } else { echo 'desktop-first-column'; } ?> ">
-            <?php
-            echo $OUTPUT->course_content_header();
-            echo $OUTPUT->main_content();
-            echo $OUTPUT->course_content_footer();
-            ?>
-        </section>
-        <?php
-            if($left == 0) {
-                echo $OUTPUT->blocks('side-post', 'span3');
-            }
-        ?>
-    </div>
-    <?php if (is_siteadmin()) { ?>
+<?php
+if ($left == 1) {
+    echo $OUTPUT->blocks('side-post', 'span3 desktop-first-column');
+}
+?>
+<section id="region-main" class="span9 <?php
+if ($left) {
+    echo ' ';
+} else {
+    echo 'desktop-first-column';
+} ?> ">
+
+<?php
+echo $OUTPUT->course_content_header();
+echo $OUTPUT->main_content();
+echo $OUTPUT->course_content_footer();
+?>
+</section>
+
+<?php
+if ($left == 0) {
+    echo $OUTPUT->blocks('side-post', 'span3');
+}
+?>
+</div>
+
+<?php
+if (is_siteadmin()) {
+?>
       <div class="hidden-blocks">
         <div class="row-fluid">
           <h4><?php echo get_string('frnt-footer', 'theme_bcu') ?></h4>
@@ -391,8 +484,10 @@ if (!empty($PAGE->theme->settings->infobox2)) {
           ?>
         </div>
       </div>
-    <?php } ?>
-    </div>
+    <?php
+}
+?>
+</div>
 
 <?php
 require_once(dirname(__FILE__) . '/includes/footer.php');

layout/grader.php
@@ -36,12 +36,19 @@ $left = theme_bcu_get_block_side();
         <?php
             echo $OUTPUT->blocks('side-post', 'span3 desktop-first-column');
         ?>
-        <section id="region-main" class="span9 <?php if ($left) { echo ' '; } else { echo 'desktop-first-column'; } ?> ">
-            <?php
-            echo $OUTPUT->course_content_header();
-            echo $OUTPUT->main_content();
-            echo $OUTPUT->course_content_footer();
-            ?>
+        <section id="region-main" class="span9 <?php
+if ($left) {
+    echo ' ';
+} else {
+    echo 'desktop-first-column';
+}
+?>">
+
+<?php
+    echo $OUTPUT->course_content_header();
+    echo $OUTPUT->main_content();
+    echo $OUTPUT->course_content_footer();
+?>
         </section>
     </div>
 </div>

layout/report.php
@@ -23,8 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
  */
-if($PAGE->pagetype=='grade-report-grader-index') {
-    if($CFG->version <= 2014111000) {
+if ($PAGE->pagetype == 'grade-report-grader-index') {
+    if ($CFG->version <= 2014111000) {
         require_once(dirname(__FILE__) .'/columns2.php');
     } else {
         require_once(dirname(__FILE__) .'/grader.php');

commit c870e648176745e3a03713634ec0947f4393267e

Fix bug introduced in b71209a which was to pass codechecker

layout/columns3.php
@@ -64,7 +64,7 @@ $left = theme_adaptable_get_block_side();
             <div class="row-fluid">                
                     <?php echo $OUTPUT->page_navbar(true); ?>                
 <?php
-if ($left == echo) {
+if ($left == 1) {
     $OUTPUT->blocks('side-post', 'span3 desktop-first-column');
 }
 ?>