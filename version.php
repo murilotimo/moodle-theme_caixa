9eb8dcb9982816207b7e76cff3f56d4b8c3f5e7a
unknown <ac1216@CUTBM3244EF.coventry.ac.uk>
2016-01-13 10:28:12 +0000

    Lower required version of bootstrapbase so it will install on Moodle 2.8 without requiring bootstrapbase upgrade. This version works with adaptable.

--- version.php
+++ version.php
@@ -32,6 +32,6 @@ $plugin->component = 'theme_adaptable';
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'theme_bootstrapbase'  => 2015103000,
+    'theme_bootstrapbase'  => 2014111000,
     'block_course_overview' => ANY_VERSION
 );
