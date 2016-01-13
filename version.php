bd03a0af164b9f8b0b1f727044bd90ce30bfa4b1
unknown <ac1216@CUTBM3244EF.coventry.ac.uk>
2016-01-13 11:52:33 +0000

    increment version for upload to moodle.org

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016010900;
+$plugin->version   = 2016011300;
 $plugin->requires  = 2013110500;
 $plugin->component = 'theme_adaptable';
 $plugin->release = '1.0.0';
