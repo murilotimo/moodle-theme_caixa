eaf7afbdc60cd3407f13d1564827a49601fdc9e1
3_bits <info@3bits.es>
2016-01-18 21:58:08 +0100

    Fix #270

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016011300;
+$plugin->version   = 2016011301;
 $plugin->requires  = 2013110500;
 $plugin->component = 'theme_adaptable';
 $plugin->release = '1.0.0';
