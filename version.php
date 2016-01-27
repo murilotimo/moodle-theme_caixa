29f618d152f8277f6cb6be8a141a124935e4e320
unknown <ac1216@CUTBM3244EF.coventry.ac.uk>
2016-01-27 18:10:19 +0000

    Update verson and changes file for release 1.1.0

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016012503;
+$plugin->version   = 2016012700;
 $plugin->requires  = 2013110500;
 $plugin->component = 'theme_adaptable';
-$plugin->release = '1.0.0';
+$plugin->release = '1.1.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2014111000,
