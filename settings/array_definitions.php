ce759e98fec935f811b704cfa35f9379ac27f208
unknown <ac1216@CUTBM3244EF.coventry.ac.uk>
2016-03-07 08:51:50 +0000

    Increase possible width for course title setting to help Fix #354

--- settings/array_definitions.php
+++ settings/array_definitions.php
@@ -836,9 +836,9 @@ $from95to100percent = array(
     '100%' => '100%',
 );
 
-$from35to50percent = array();
-for ($i = 35; $i < 51; $i++) {
-    $from35to50percent[$i . '%'] = $i . '%';
+$from35to80percent = array();
+for ($i = 35; $i < 81; $i++) {
+    $from35to80percent[$i . '%'] = $i . '%';
 }
 
 $from0to60inc5 = array();
