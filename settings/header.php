f752d6f17201dbb67dcffb918d2e9ae5af5f09e3
fernando <info@3bits.es>
2016-07-03 21:25:35 +0200

    Fix #472

--- settings/header.php
+++ settings/header.php
@@ -137,7 +137,7 @@
     $name = 'theme_adaptable/coursetitlemaxwidth';
     $title = get_string('coursetitlemaxwidth', 'theme_adaptable');
     $description = get_string('coursetitlemaxwidthdesc', 'theme_adaptable');
-    $setting = new admin_setting_configselect($name, $title, $description, '50%', $from35to80percent);
+    $setting = new admin_setting_configselect($name, $title, $description, '50%', $from35to100percent);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $temp->add($setting);
 
