8abe456ad2c0983e5a5bbe4893b08a0033f2e228
fernando <info@3bits.es>
2017-02-08 11:07:01 +0100

    Version update

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017012300;
+$plugin->version   = 2017020800;
 $plugin->requires  = 2015111600;
 $plugin->component = 'theme_adaptable';
-$plugin->release = '1.3.1.1';
+$plugin->release = '1.3.1.1+';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2014111000,
