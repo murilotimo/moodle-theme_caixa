ffe687bcf0472773888b04e2350ab69da157f46b
Bas Brands <bas@sonsbeekmedia.nl>
2017-04-05 15:57:24 +0200

    #650 improve-course-section-navigation

--- renderers.php
+++ renderers.php
@@ -31,8 +31,214 @@ require_once($CFG->dirroot.'/blocks/course_overview/locallib.php');
 require_once($CFG->dirroot.'/course/renderer.php');
 require_once($CFG->libdir.'/coursecatlib.php');
 require_once($CFG->dirroot.'/message/lib.php');
+require_once($CFG->dirroot.'/course/format/topics/renderer.php');
+require_once($CFG->dirroot.'/course/format/weeks/renderer.php');
 
+trait single_section_page {
+    /**
+     * Output the html for a single section page .
+     *
+     * @param stdClass $course The course entry from DB
+     * @param array $sections (argument not used)
+     * @param array $mods (argument not used)
+     * @param array $modnames (argument not used)
+     * @param array $modnamesused (argument not used)
+     * @param int $displaysection The section number in the course which is being displayed
+     */
+    public function print_single_section_page($course, $sections, $mods, $modnames, $modnamesused, $displaysection) {
+        global $PAGE;
 
+        $modinfo = get_fast_modinfo($course);
+        $course = course_get_format($course)->get_course();
+
+        // Can we view the section in question?
+        if (!($sectioninfo = $modinfo->get_section_info($displaysection))) {
+            // This section doesn't exist
+            print_error('unknowncoursesection', 'error', null, $course->fullname);
+            return;
+        }
+
+        if (!$sectioninfo->uservisible) {
+            if (!$course->hiddensections) {
+                echo $this->start_section_list();
+                echo $this->section_hidden($displaysection, $course->id);
+                echo $this->end_section_list();
+            }
+            // Can't view this section.
+            return;
+        }
+
+        // Copy activity clipboard..
+        echo $this->course_activity_clipboard($course, $displaysection);
+        $thissection = $modinfo->get_section_info(0);
+        if ($thissection->summary or !empty($modinfo->sections[0]) or $PAGE->user_is_editing()) {
+            echo $this->start_section_list();
+            echo $this->section_header($thissection, $course, true, $displaysection);
+            echo $this->courserenderer->course_section_cm_list($course, $thissection, $displaysection);
+            echo $this->courserenderer->course_section_add_cm_control($course, 0, $displaysection);
+            echo $this->section_footer();
+            echo $this->end_section_list();
+        }
+
+        // Start single-section div
+        echo html_writer::start_tag('div', array('class' => 'single-section'));
+
+        // The requested section page.
+        $thissection = $modinfo->get_section_info($displaysection);
+
+        // Title with section navigation links.
+        $sectionnavlinks = $this->get_nav_links($course, $modinfo->get_section_info_all(), $displaysection);
+        $sectiontitle = '';
+        $sectiontitle .= html_writer::start_tag('div', array('class' => 'section-navigation navigationtitle'));
+        // Title attributes
+        $classes = 'sectionname';
+        if (!$thissection->visible) {
+            $classes .= ' dimmed_text';
+        }
+        $sectionname = html_writer::tag('span', $this->section_title_without_link($thissection, $course));
+        $sectiontitle .= $this->output->heading($sectionname, 3, $classes);
+
+        $sectiontitle .= html_writer::end_tag('div');
+        echo $sectiontitle;
+
+        // Now the list of sections..
+        echo $this->start_section_list();
+
+        echo $this->section_header($thissection, $course, true, $displaysection);
+        // Show completion help icon.
+        $completioninfo = new completion_info($course);
+        echo $completioninfo->display_help_icon();
+
+        echo $this->courserenderer->course_section_cm_list($course, $thissection, $displaysection);
+        echo $this->courserenderer->course_section_add_cm_control($course, $displaysection, $displaysection);
+        echo $this->section_footer();
+        echo $this->end_section_list();
+
+        // Display section bottom navigation.
+        $sectionbottomnav = '';
+        $sectionbottomnav .= html_writer::start_tag('nav', array('class' => 'section_footer'));
+        $sectionbottomnav .= $sectionnavlinks['previous'];
+        $sectionbottomnav .= $sectionnavlinks['next'];
+        $sectionbottomnav .= html_writer::tag('div', '', array('class' => 'clearfix'));
+        $sectionbottomnav .= html_writer::end_tag('nav');
+        $sectionbottomnav .= html_writer::tag('div', $this->section_nav_selection($course, $sections, $displaysection), array('class' => 'jumpnav'));
+        echo $sectionbottomnav;
+
+        // Close single-section div.
+        echo html_writer::end_tag('div');
+    }
+
+    /**
+     * Generate the html for the 'Jump to' menu on a single section page.
+     *
+     * @param stdClass $course The course entry from DB
+     * @param array $sections The course_sections entries from the DB
+     * @param $displaysection the current displayed section number.
+     *
+     * @return string HTML to output.
+     */
+    protected function section_nav_selection($course, $sections, $displaysection) {
+        global $CFG;
+        $o = '';
+        $sectionmenu = array();
+        $sectionmenu[course_get_url($course)->out(false)] = get_string('maincoursepage');
+        $modinfo = get_fast_modinfo($course);
+        $section = 1;
+        while ($section <= $course->numsections) {
+            $thissection = $modinfo->get_section_info($section);
+            $showsection = $thissection->uservisible or !$course->hiddensections;
+            if (($showsection) && ($section != $displaysection) && ($url = course_get_url($course, $section))) {
+                $sectionmenu[$url->out(false)] = $this->shorten_string(get_section_name($course, $section));
+            }
+            $section++;
+        }
+
+        $select = new url_select($sectionmenu, '', array('' => get_string('jumpto')));
+        $select->class = 'jumpmenu';
+        $select->formid = 'sectionmenu';
+        $o .= $this->output->render($select);
+
+        return $o;
+    }
+
+    private function shorten_string($string, $ellipsis = '..') {
+        $maxlen = 50;
+        $string = strip_tags($string);
+        $boundary = $maxlen - strlen($ellipsis);
+        if ((strlen($string) > $maxlen)) {
+            $shortstring = substr($string, 0, $boundary) . $ellipsis;
+        } else {
+            $shortstring = $string;
+        }
+        return $shortstring;
+    }
+
+    /**
+     * Generate next/previous section links for naviation
+     *
+     * @param stdClass $course The course entry from DB
+     * @param array $sections The course_sections entries from the DB
+     * @param int $sectionno The section number in the coruse which is being dsiplayed
+     * @return array associative array with previous and next section link
+     */
+    protected function get_nav_links($course, $sections, $sectionno) {
+        // FIXME: This is really evil and should by using the navigation API.
+        $course = course_get_format($course)->get_course();
+        $canviewhidden = has_capability('moodle/course:viewhiddensections', context_course::instance($course->id))
+            or !$course->hiddensections;
+
+        $links = array('previous' => html_writer::tag('div', '', array('class' => 'previous_section prevnext')),
+            'next' => html_writer::tag('div', '', array('class' => 'next_section prevnext')));
+        $back = $sectionno - 1;
+
+        $hasprev = $hasnext = false;
+        while ($back > 0 && !$hasprev) {
+            if ($canviewhidden || $sections[$back]->uservisible) {
+                $params = array();
+                $params['class'] = 'previous_section prevnext';
+                if (!$sections[$back]->visible) {
+                    $params['class'] = 'previous_section prevnext dimmed_text';
+                }
+                $previouslink = html_writer::tag('span', '<i class="fa fa-angle-double-left"></i>', array('class' => 'nav_icon'));
+                $sectionname = html_writer::tag('span', get_string('previoussection', 'theme_adaptable'), array('class' => 'nav_guide')) . '<br>';
+                $sectionname .= get_section_name($course, $sections[$back]);
+                $previouslink .= html_writer::tag('span', $sectionname, array('class' => 'text'));
+                $links['previous'] = html_writer::link(course_get_url($course, $back), $previouslink, $params);
+                $hasprev = true;
+            }
+            $back--;
+        }
+
+        $forward = $sectionno + 1;
+        while ($forward <= $course->numsections && !$hasnext) {
+            if ($canviewhidden || $sections[$forward]->uservisible) {
+                $params = array();
+                $params['class'] = 'next_section prevnext';
+                if (!$sections[$forward]->visible) {
+                    $params['class'] = 'next_section prevnext dimmed_text';
+                }
+                
+                $sectionname = html_writer::tag('span', get_string('nextsection', 'theme_adaptable'), array('class' => 'nav_guide')) . '<br>';
+                $sectionname .= get_section_name($course, $sections[$forward]);
+                $nextlink = html_writer::tag('span', $sectionname, array('class' => 'text'));
+                $nextlink .= html_writer::tag('span', '<i class="fa fa-angle-double-right"></i>', array('class' => 'nav_icon'));
+                $links['next'] = html_writer::link(course_get_url($course, $forward), $nextlink, $params);
+                $hasnext = true;
+            }
+            $forward++;
+        }
+
+        return $links;
+    }
+}
+
+class theme_adaptable_format_topics_renderer extends format_topics_renderer {
+    use single_section_page;
+}
+
+class theme_adaptable_format_weeks_renderer extends format_topics_renderer {
+    use single_section_page;
+}
 
 /******************************************************************************************
  * @copyright 2015 Jeremy Hopkins (Coventry University)
@@ -2523,3 +2729,5 @@ class theme_adaptable_core_course_renderer extends core_course_renderer {
 
     // End.
 }
+
+
