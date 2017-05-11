27cd2f38847a7fa33af7fd234325217820322d1f
root <info@3bits.es>
2017-05-11 16:48:30 +0200

    Revert wrong strings in es and ca

--- lang/ca/theme_adaptable.php
+++ lang/ca/theme_adaptable.php
@@ -125,7 +125,6 @@ $string['blocksettings'] = 'Blocs';
 $string['frontpagealertsettings'] = 'Alertes';
 $string['frontpagealertboxssettings'] = 'Configuració del quadre d&#39;alerta';
 $string['frontpageblockregionsettings'] = 'Creador de regions per a blocs';
-$string['dashboardblockregionsettings'] = 'Creador de regions Panell de control per a blocs';
 $string['frontpageblocksettings'] = 'Blocs promocionals';
 $string['frontpagetickersettings'] = 'Anuncis';
 $string['frontpageslidersettings'] = 'Presentació diapositives';
@@ -260,11 +259,6 @@ $string['blocklayoutbuilderdesc'] = 'A continuació pots crear la teva pròpia d
 Per afegir contingut a aquestes regions necessitaràs <strong>Activar edició a la pàgina principal de Moodle</strong>.
 Llavors podràs arrossegar i deixar anar els blocs a les regions creades.';
 
-$string['dash_blocklayoutbuilder'] = 'Creador de regions panell de control per a blocs';
-$string['dash_blocklayoutbuilderdesc'] = 'A continuació pots crear la teva pròpia distribució de regions panell de control per als blocs de moodle a la pàgina principal.
-Per afegir contingut a aquestes regions necessitaràs <strong>Activar edició a la pàgina principal de Moodle</strong>.
-Llavors podràs arrossegar i deixar anar els blocs a les regions creades.';
-
 $string['blocklayoutlayoutcheck'] = 'Comprova la distribució';
 $string['blocklayoutlayoutcheckdesc'] = 'Utilitza aquesta opció per veure la distribució creada i comprovar el nombre de blocs que has utilitzat.';
 $string['blocklayoutlayoutcount1'] = 'Pots crear un màxim de ';
@@ -273,15 +267,8 @@ $string['blocklayoutlayoutcount2'] = ' regions per a blocs. Ara estàs utilitzan
 $string['blocklayoutlayoutrow'] = 'Files de regions per a blocs';
 $string['blocklayoutlayoutrowdesc'] = 'Afegeix / estableix la distribució de files de les regions per a blocs de la pàgina principal.';
 
-$string['dashblocklayoutlayoutrow'] = 'Files de regions panell de control per a blocs';
-$string['dashblocklayoutlayoutrowdesc'] = 'Afegeix / estableix la distribució de files de les regions panell de control per a blocs de la pàgina principal.';
-
 $string['frontpageblocksenabled'] = 'Activa les regions per a blocs a la pàgina principal';
 $string['frontpageblocksenableddesc'] = 'Activa o desactiva les regions per a blocs de la pàgina principal.
-Pots arrossegar i deixar anar els blocs a les regions creades.';
-
-$string['dashblocksenabled'] = 'Activa les regions panell de control per a blocs a la pàgina principal';
-$string['dashblocksenableddesc'] = 'Activa o desactiva les regions panell de control per a blocs de la pàgina principal.
 Pots arrossegar i deixar anar els blocs a les regions creades.';
 
 $string['layoutcheck'] = 'Comprova la distribució';
@@ -939,7 +926,7 @@ $string['tilesshowallcontactsdesc'] = 'Mostra tots els contactes del curs o nom�
 $string['course'] = 'Curs';
 
 
-// Alerts **********************************************************.
+// Alerts **********************************************************
 // Alert message if acting as other role.
 $string['actingasrole'] = 'Estàs actuant actualment com un paper diferent';
 
